c4a47b02ddd7b5906e80e80b9f38e475e3f57762
Aliyu M. Lawal <alfadaguru@gmail.com>
2021-11-07 05:00:57 +0400

    Update config.js

--- helper/config.js
+++ helper/config.js
@@ -40,7 +40,7 @@ const config = {
     site_email: '',
     secret_key: 'jfVRtwN7xBl7LjRucIUdPnrh1UVUhzhZ',
     public_key: '6gluXXunc77uukLJbSmlQ31ckSlLq8Qi',
-    eth_http: "https://rinkeby.infura.io/v3/64fa77a39b9a4c31b186fb2148edff70",
+    eth_http: "https://rinkeby.infura.io/v3/552872ab900243aaa3774eeb8cc912c4",
 };
 
 
