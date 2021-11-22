ee346ae417259dd0ceb94e35c7bc901813ac0e64
MusahMusah <shehialiyu0@gmail.com>
2021-11-22 14:15:28 +0100

    Updates added to unpublish an item

--- module/item/controller/itemController.js
+++ module/item/controller/itemController.js
@@ -153,7 +153,7 @@ exports.unpublish = function(req, res) {
         });
         return;
     }
-    items.findOne({ _id: req.body.item_id, author_id: req.decoded.user_id, status: "active" }).populate('collection_id').exec(function(err, item) {
+    items.findOne({ _id: req.body.item_id, author_id: req.decoded.user_id, status: "inactive" }).populate('collection_id').exec(function(err, item) {
         if (err || !item) {
             res.json({
                 status: false,
